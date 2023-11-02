31349ac7bf4baf86917d26200f5112b4d818ecdf
DRoy01 <debasishroy304@gmail.com>
2023-11-02 22:28:26 +0600

    Cube follows the cusror

--- Practice1/src/script.js
+++ Practice1/src/script.js
@@ -1,15 +1,18 @@
 import './style.css'
 import * as THREE from 'three'
 
-//Scene
-const scene = new THREE.Scene()
 
-
-//Object
-const Cube = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0x00ffef })
-const mesh = new THREE.Mesh(Cube, material)
-scene.add(mesh)
+//Cursor
+const cursor = {
+    x: 0, 
+    y: 0
+}
+window.addEventListener('mousemove', (event) =>
+{
+    cursor.x = event.clientX
+    cursor.y = event.clientY
+    console.log(event.clientX, event.clientY)
+})
 
 //Sizes
 const sizes = {
@@ -17,16 +20,44 @@ const sizes = {
     height: 600
 }
 
+//Canvas
+const canvas = document.querySelector('.webgl')
+
+//Scene
+const scene = new THREE.Scene()
+
+//Object (Shape, Material = Mesh)
+const cube = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
+const material = new THREE.MeshBasicMaterial({ color: 0x00ffaf })
+const mesh = new THREE.Mesh(cube, material)
+scene.add(mesh)
+
 //Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+const camera1 = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera1.position.z = 5
+scene.add(camera1)
 
 //Renderer
-const canvas = document.querySelector('.webgl')
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+//Clock
+const clock = new THREE.Clock()
+
+//Animation
+
+const tick = () => 
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    mesh.position.x = (cursor.x / 100) - 4
+    mesh.position.y = 4 - (cursor.y / 100)
+
+    renderer.render(scene, camera1)
+    window.requestAnimationFrame(tick)
+}
+
+tick()
